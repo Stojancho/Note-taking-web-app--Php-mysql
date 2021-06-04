ae14aeae78c9caf6f73f2e3667727d93325e7ee6
Stojancho Ggjorgjiev <100janGee@gmail.com>
2021-06-04 09:50:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ include('server.php');
 		$note = $record['note'];
 		$id = $record['id'];
 	}
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -22,72 +21,59 @@ include('server.php');
 	<link rel = "stylesheet" type = "text/css" href = "style.css?<?php echo time(); ?>" />
 </head>
 <body>
-<div class = "glass">
-
-<form method = "post" action ="server.php">
-		<input type = "hidden" name = "id" value ="<?php echo $id; ?>">  
-	
+	<div class = "glass">
+		<form method = "post" action ="server.php">
+			<input type = "hidden" name = "id" value ="<?php echo $id; ?>">  
 			<div class = "input-group">
 				<label>Title</label>
 				<input type = "text" name = "title" value = "<?php echo $title; ?>" placeholder = "Enter Titel">
 			</div>
-		
 			<div class = "input-group">
 				<label>Note</label>
 				<input type = "text" name = "note" value = "<?php echo $note; ?>" placeholder = "Enter Note">
 			<div class = "input-group">
-		
-		<?php if ($edit_state == false): ?>
-			<button type = "submit" name ="save" class = "btn" >Save</button>
-		<?php else :?>
-			<button type = "submit" name ="edit" class = "btn" >Edit</button>
-		<?php endif ?>
-		
-			
-		</div>	
-		</div>
-	</form>
-	<?php if (isset($_SESSION['msg'])): ?>
-		<div class = 'msg'>
-			<?php
-				echo $_SESSION['msg'];
-				unset($_SESSION['msg']);
-			?>
-		</div>
-		
-		
-	
-	<?php endif ?>	
-	<table>
-	
-		<thead>
-			<tr>
-				<th>Title</th>
-				<th>Note</th>
-				<th colspan="2">Action</th>	
-			</tr>
-			
-		</thead>
-		
-		<tbody>
-			<?php while ($row = mysqli_fetch_array($results)){ ?>
+				
+			<?php if ($edit_state == false): ?>
+				<button type = "submit" name ="save" class = "btn" >Save</button>
+			<?php else :?>
+				<button type = "submit" name ="edit" class = "btn" >Edit</button>
+			<?php endif ?>	
+			</div>	
+			</div>
+		</form>
+		<?php if (isset($_SESSION['msg'])): ?>
+			<div class = 'msg'>
+				<?php
+					echo $_SESSION['msg'];
+					unset($_SESSION['msg']);
+				?>
+			</div>
+		<?php endif ?>	
+		<table>
+			<thead>
 				<tr>
-					<td><?php echo $row['title']; ?></td>
-					<td><?php echo $row['note']; ?></td>
-					<td>
-						<a class= "edit_btn" href = "index.php?edit=<?php echo $row['id']; ?>">Edit</a>
-					</td>
-					<td>
-						<a class = "del_btn" href = "server.php?del=<?php echo $row['id']; ?>">Delete</a>
-					</td>
-				</tr>
-			<?php } ?>
-		
-		</tbody>		
-	</table>
-	
-	
-</div>	
+					<th>Title</th>
+					<th>Note</th>
+					<th colspan="2">Action</th>	
+				</tr>		
+			</thead>
+			<tbody>
+				<?php while ($row = mysqli_fetch_array($results)){ ?>
+					<tr>
+						<td><?php echo $row['title']; ?></td>
+						<td><?php echo $row['note']; ?></td>
+						<td>
+							<a class= "edit_btn" href = "index.php?edit=<?php echo $row['id']; ?>">Edit</a>
+						</td>
+						<td>
+							<a class = "del_btn" href = "server.php?del=<?php echo $row['id']; ?>">Delete</a>
+						</td>
+					</tr>
+				<?php } ?>	
+			</tbody>		
+		</table>	
+	</div>	
+
 </body>
 
 
